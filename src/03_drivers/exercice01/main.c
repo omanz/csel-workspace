6778185e9a12711d9599b75cf39ee8473079099c
Olivia Manz <olivia.manz@master.hes-so.ch>
2026-03-26 16:11:12 +0100

    driver ex 1: restrict access

--- src/03_drivers/exercice01/main.c
+++ src/03_drivers/exercice01/main.c
@@ -9,30 +9,32 @@
 
 int main()
 {
-    /* open memory file descriptor */
+    /* Ouverture du fichier correspondant au pilote */
     int fd = open("/dev/mem", O_RDWR);
     if (fd < 0) {
         printf("Could not open /dev/mem: error=%i\n", fd);
         return -1;
     }
-
+    /* placer dans la mem virt du processeur les registres du periph */
     size_t psz     = getpagesize();
     off_t dev_addr = 0x01c14200;
-    off_t ofs      = dev_addr % psz;
-    off_t offset   = dev_addr - ofs;
-    printf(
-        "psz=%lx, addr=%lx, offset=%lx, ofs=%lx\n", psz, dev_addr, offset, ofs);
+    off_t ofs      = dev_addr % psz;    // difference entre le debut de la page et la plage mem
+    off_t offset   = dev_addr - ofs;    // adresse de debut de page
 
-    /* map to user space nanopi internal registers */
     volatile uint32_t* regs =
-        mmap(0, psz, PROT_READ | PROT_WRITE, MAP_SHARED, fd, offset);
+    mmap (NULL,         // adr de depart en memoire
+        psz,            // taille de zone a placer en mem virtuelle (multiple de taille de page)
+        PROT_READ,      // droit acces a la memoire
+        MAP_PRIVATE,    // visibilité de la page
+        fd,
+        offset);        // offset des registres en memoire phyique
 
-    if (regs == MAP_FAILED)  // (void *)-1
-    {
+    if (regs == MAP_FAILED) {
         printf("mmap failed, error: %i:%s \n", errno, strerror(errno));
         return -1;
     }
 
+    // info tout les 4, ce sera le pas
     uint32_t chipid[4] = {
         [0] = *(regs + (ofs + 0x00) / sizeof(uint32_t)),
         [1] = *(regs + (ofs + 0x04) / sizeof(uint32_t)),
